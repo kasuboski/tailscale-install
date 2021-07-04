5c0f371b72440ee561f8ef3d474dad34d87efe29
Josh Kasuboski <josh.kasuboski@gmail.com>
2021-07-04 17:42:31 -0500

    use facts for generic install 🎉

--- install.py
+++ install.py
@@ -1,15 +1,20 @@
 from pyinfra import host
 from pyinfra.operations import apt, server
+from pyinfra.facts.server import LinuxDistribution
+
+linux_distribution = host.get_fact(LinuxDistribution)
+linux_name = linux_distribution['release_meta']['ID']
+linux_codename = linux_distribution['release_meta']['CODENAME']
 
 # Adding tailscale for raspberry pi
 apt.key(
   name='Add tailscale key',
-  src='https://pkgs.tailscale.com/stable/raspbian/buster.gpg',
+  src='https://pkgs.tailscale.com/stable/{}/{}.gpg'.format(linux_name, linux_codename),
 )
 
 apt.repo(
   name='Add tailscale repo',
-  src='deb https://pkgs.tailscale.com/stable/raspbian buster main',
+  src='deb https://pkgs.tailscale.com/stable/{} {} main'.format(linux_name, linux_codename),
   filename='tailscale',
 )
 
